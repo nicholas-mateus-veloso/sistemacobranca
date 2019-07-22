438d6964aa2bf7e2b1edd63cf08ff161c13ced89
Karasurage <31319694+karasurage@users.noreply.github.com>
2019-07-22 14:47:31 -0300

    Initial commit

---
+++ src/main/java/com/springboot/cobranca/controller/TituloController.java
@@ -0,0 +1,92 @@
+package com.springboot.cobranca.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.springboot.cobranca.model.StatusTitulo;
+import com.springboot.cobranca.model.Titulo;
+import com.springboot.cobranca.repository.filter.TituloFilter;
+import com.springboot.cobranca.service.CadastroTituloService;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+
+	@Autowired
+	private CadastroTituloService cadastroTituloService;
+
+	// Mapear a Página de Cadastro de Novo Título
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(new Titulo());
+		return mv;
+	}
+
+	// Parâmetro para Salvar o Título e fazer a validação do campo para salvar
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
+
+		try {
+			cadastroTituloService.salvar(titulo);
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch (IllegalArgumentException e) {
+			errors.rejectValue("dataVencimento", null, e.getMessage());
+			return CADASTRO_VIEW;
+		}
+	}
+
+	// Parâmetro para Mapear e fazer a validação do campo de pesquisa
+	@RequestMapping
+	public ModelAndView pesquisar(@ModelAttribute("filtro") TituloFilter filtro) {
+		List<Titulo> todosTitulos = cadastroTituloService.filtrar(filtro);
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+
+	// Parâmetro para Mapear e fazer a Edição dos campos de Título já salvos no BD
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+
+	// Parâmentro para executar a exclusão do título de acordo com seu código
+	// cadastrado
+	@RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		cadastroTituloService.excluir(codigo);
+		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
+	}
+
+	@RequestMapping(value = "/{codigo}/receber", method = RequestMethod.PUT)
+	public @ResponseBody String receber(@PathVariable Long codigo) {
+		return cadastroTituloService.receber(codigo);
+	}
+
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
+	}
+}
---
+++ src/main/java/com/springboot/cobranca/service/CadastroTituloService.java
@@ -0,0 +1,44 @@
+package com.springboot.cobranca.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.springboot.cobranca.model.StatusTitulo;
+import com.springboot.cobranca.model.Titulo;
+import com.springboot.cobranca.repository.Titulos;
+import com.springboot.cobranca.repository.filter.TituloFilter;
+
+@Service
+public class CadastroTituloService {
+
+	@Autowired
+	private Titulos titulos;
+
+	public void salvar(Titulo titulo) {
+		try {
+			titulos.save(titulo);
+		} catch (DataIntegrityViolationException e) {
+			throw new IllegalArgumentException("Formato de data inválido");
+		}
+	}
+
+	public void excluir(Long codigo) {
+		titulos.delete(codigo);
+	}
+
+	public String receber(Long codigo) {
+		Titulo titulo = titulos.findOne(codigo);
+		titulo.setStatus(StatusTitulo.RECEBIDO);
+		titulos.save(titulo);
+
+		return StatusTitulo.RECEBIDO.getDescricao();
+	}
+	
+	public List<Titulo> filtrar(TituloFilter filtro) {
+		String descricao = filtro.getDescricao() == null ? "%" : filtro.getDescricao();
+		return titulos.findByDescricaoContaining(descricao);
+	}
+}
